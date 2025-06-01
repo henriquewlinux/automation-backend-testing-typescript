b570c3d04fe4b511b8916822160ddc4148cec3a4
Henrique Silveira <henriquewlinux+git@gmail.com>
2025-05-31 22:01:34 -0300

    correction package.json

--- .mocharc.js
+++ .mocharc.js
@@ -1,5 +1,5 @@
 module.exports = {
     reporter: 'mochawesome',
     reporterOptions: 'json=false,reportDir=../../report,reportFilename=APITestsReport,reportTitle=RestFul-booker,charts=true,code=false',
-    spec: ["build/test/*.js"]
+    spec: ["build/tests/*.js"]
 }
